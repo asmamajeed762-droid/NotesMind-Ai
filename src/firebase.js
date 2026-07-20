6b24aa70aaf5c37e02f18e3fb3a1b104b7bd3156
asimamaj <asimamajeed768@gmail.com>
2026-07-20 20:00:57 +0500

    Update project

--- src/firebase.js
+++ src/firebase.js
@@ -1,42 +1,20 @@
 import { initializeApp } from "firebase/app";
-
-import {
-  getAuth,
-  GoogleAuthProvider
-} from "firebase/auth";
-
-import {
-  getFirestore
-} from "firebase/firestore";
-
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyCj__AhFEE62DrZTvsn4H0zM6lRNBBtLic",
-
+  apiKey: "AIzaSyB5LV81bV4fL953iog5jIqcq_kqinJUtaI",
   authDomain: "notesmind--ai.firebaseapp.com",
-
   projectId: "notesmind--ai",
-
   storageBucket: "notesmind--ai.firebasestorage.app",
-
   messagingSenderId: "709152132066",
-
-  appId: "1:709152132066:web:cc059d0700ad7567e747bb",
-
-  measurementId: "G-Q9ZSQ27L5Q"
-
+  appId: "1:709152132066:web:401757c08f710d33e747bb",
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-
-export const auth = getAuth(app);
-
-export const db = getFirestore(app);
-
-export const googleProvider = new GoogleAuthProvider();
+export { auth, db, googleProvider };
